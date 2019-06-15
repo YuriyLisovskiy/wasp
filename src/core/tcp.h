009c76be59997accc11fd7dcbd9577f82a01733a
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-06-15 23:22:48 +0300

    added platform-specific definitions, globals and cmake script

---
+++ src/core/tcp.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Yuriy Lisovskiy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * tcp/ip server definition.
+ */
+
+#ifndef LEAF_TCP_H
+#define LEAF_TCP_H
+
+#include "../globals.h"
+
+__INTERNAL_BEGIN__
+
+// TODO:
+
+__INTERNAL_END__
+
+#endif // LEAF_TCP_H
