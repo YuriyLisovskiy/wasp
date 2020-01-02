d4f40184fd474e5e9507511c58a8629677b07f48
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2020-01-03 00:59:03 +0200

    fix mem leack warning

--- tests/mem_leak_check.h
+++ tests/mem_leak_check.h
@@ -15,7 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#ifndef MEM_LEAK_CHECK_H
+#define MEM_LEAK_CHECK_H
 
 #include <map>
 #include <memory>
@@ -115,3 +116,6 @@ void operator delete(void* mem) throw()
 }
 
 #endif // DETECT_MEMORY_LEAK
+
+
+#endif // MEM_LEAK_CHECK_H
